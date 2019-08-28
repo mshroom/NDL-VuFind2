cbd98b707d9f52159fafd0ee2c5b627790e45b87
Ere Maijala <ere.maijala@helsinki.fi>
2019-08-28 15:25:53 +0300

    Partially working update.

--- module/Finna/src/Finna/ILS/Driver/Alma.php
+++ module/Finna/src/Finna/ILS/Driver/Alma.php
@@ -178,6 +178,262 @@ class Alma extends \VuFind\ILS\Driver\Alma
     }
 
     /**
+     * Get Patron Profile
+     *
+     * This is responsible for retrieving the profile for a specific patron.
+     *
+     * @param array $patron The patron array
+     *
+     * @return array Array of the patron's profile data on success.
+     */
+    public function getMyProfile($patron)
+    {
+        $patronId = $patron['id'];
+        $xml = $this->makeRequest('/users/' . $patronId);
+        if (empty($xml)) {
+            return [];
+        }
+        $profile = [
+            'firstname'  => isset($xml->first_name)
+                                ? (string)$xml->first_name
+                                : null,
+            'lastname'   => isset($xml->last_name)
+                                ? (string)$xml->last_name
+                                : null,
+            'group'      => isset($xml->user_group['desc'])
+                                ? (string)$xml->user_group['desc']
+                                : null,
+            'group_code' => isset($xml->user_group)
+                                ? (string)$xml->user_group
+                                : null
+        ];
+        $contact = $xml->contact_info;
+        if ($contact) {
+            if ($contact->addresses) {
+                $address = null;
+                foreach ($contact->addresses->address as $item) {
+                    if ('true' === (string)$item['preferred']) {
+                        $address = $item;
+                        break;
+                    }
+                }
+                if (null === $address) {
+                    $address = $contact->addresses[0]->address[0];
+                }
+                $profile['address1'] =  isset($address->line1)
+                                            ? (string)$address->line1
+                                            : null;
+                $profile['address2'] =  isset($address->line2)
+                                            ? (string)$address->line2
+                                            : null;
+                $profile['address3'] =  isset($address->line3)
+                                            ? (string)$address->line3
+                                            : null;
+                $profile['zip']      =  isset($address->postal_code)
+                                            ? (string)$address->postal_code
+                                            : null;
+                $profile['city']     =  isset($address->city)
+                                            ? (string)$address->city
+                                            : null;
+                $profile['country']  =  isset($address->country)
+                                            ? (string)$address->country
+                                            : null;
+            }
+            if ($contact->phones) {
+                $phone = null;
+                foreach ($contact->phones->phone as $item) {
+                    if ('true' === (string)$item['preferred']) {
+                        $phone = $item;
+                        break;
+                    }
+                }
+                if (null === $phone) {
+                    $phone = $contact->phones[0]->phone[0];
+                }
+                $profile['phone'] = isset($phone->phone_number)
+                                        ? (string)$phone->phone_number
+                                        : null;
+            }
+            if ($contact->emails) {
+                $email = null;
+                foreach ($contact->emails->email as $item) {
+                    if ('true' === (string)$item['preferred']) {
+                        $email = $item;
+                        break;
+                    }
+                }
+                if (null === $email) {
+                    $email = $contact->emails[0]->email[0];
+                }
+                $profile['email'] = isset($email->email_address)
+                                        ? (string)$email->email_address
+                                        : null;
+            }
+        }
+
+        // Cache the user group code
+        $cacheId = 'alma|user|' . $patronId . '|group_code';
+        $this->putCachedData($cacheId, $profile['group_code'] ?? null);
+
+        return $profile;
+    }
+
+    /**
+     * Update patron contact information
+     *
+     * @param array $patron  Patron array
+     * @param array $details Associative array of patron contact information
+     *
+     * @throws ILSException
+     *
+     * @return array Associative array of the results
+     */
+    public function updateAddress($patron, $details)
+    {
+        $addressMapping = [
+            'address1' => 'line1',
+            'address2' => 'line2',
+            'address3' => 'line3',
+            'address4' => 'line4',
+            'address5' => 'line5',
+            'zip' => 'zip',
+            'city' => 'city',
+            'country' => 'country'
+        ];
+        $phoneMapping = [
+            'phone' => 'phone_number'
+        ];
+        $emailMapping = [
+            'email' => 'email_address'
+        ];
+        // We need to process address fields, phone number fields and email fields
+        // as separate sets, so divide them now to gategories
+        $hasAddress = false;
+        $hasPhone = false;
+        $hasEmail = false;
+        $fieldConfig = isset($this->config['updateAddress']['fields'])
+            ? $this->config['updateAddress']['fields'] : [];
+        foreach ($fieldConfig as $field) {
+            $parts = explode(':', $field);
+            if (isset($parts[1])) {
+                $fieldName = $parts[1];
+                if (isset($addressMapping[$fieldName])) {
+                    if (isset($details[$fieldName])) {
+                        $hasAddress = true;
+                    }
+                } elseif ('phone' === $fieldName) {
+                    if (isset($details[$fieldName])) {
+                        $hasPhone = true;
+                    }
+                } elseif ('email' === $fieldName) {
+                    $emailFields[$fieldName] = $parts[0];
+                    if (isset($details[$fieldName])) {
+                        $hasEmail = true;
+                    }
+                } else {
+                    $otherFields[$fieldName] = $parts[0];
+                }
+            }
+        }
+
+        // Retrieve old data first
+        $userData = $this->makeRequest('/users/' . $patron['id']);
+
+        $contact = $userData->contact_info ?? $userData->addChild('contact_info');
+
+        // Pick the configured fields from the request
+        if ($hasAddress) {
+            // Try to find an existing address to modify
+            $types = null;
+            if (!$contact->addresses) {
+                $contact->addChild('addresses');
+            }
+            foreach ($contact->addresses->address as $item) {
+                if ('true' === (string)$item['preferred']) {
+                    // Remove the existing address
+                    $types = clone $item->address_types->address_type;
+                    unset($item[0]);
+                    break;
+                }
+            }
+            $address = $contact->addresses->addChild('address');
+            if (null === $types) {
+                $types = $address->addChild('address_types');
+                $types->addChild('address_type', 'home');
+            } else {
+                $addressTypes = $address->addChild('address_types');
+                foreach ($types as $type) {
+                    $addressTypes->addChild('address_type', (string)$type);
+                }
+            }
+            $address['preferred'] = 'true';
+            foreach ($details as $key => $value) {
+                if (isset($addressMapping[$key])) {
+                    $address->addChild($addressMapping[$key], $value);
+                }
+            }
+        }
+
+        if ($hasPhone) {
+            // Try to find an existing phone to modify
+            $types = null;
+            if (!$contact->phones) {
+                $contact->addChild('phones');
+            }
+            foreach ($contact->phones->phone as $item) {
+                if ('true' === (string)$item['preferred']) {
+                    // Remove the existing phone number
+                    $types = clone $item->phone_types->phone_type;
+                    unset($item[0]);
+                    break;
+                }
+            }
+            $phone = $contact->phones->addChild('phone');
+            if (null === $types) {
+                $types = $phone->addChild('phone_types');
+                $types->addChild('phone_type', 'mobile');
+            } else {
+                $phoneTypes = $phone->addChild('phone_types');
+                foreach ($types as $type) {
+                    $phoneTypes->addChild('phone_type', (string)$type);
+                }
+            }
+            $phone['preferred'] = 'true';
+            foreach ($details as $key => $value) {
+                if (isset($phoneMapping[$key])) {
+                    $phone->addChild($phoneMapping[$key], $value);
+                }
+            }
+        }
+
+        // Remove data that we don't ever update and is handled by Alma as complete
+        // entities
+        unset($userData->user_identifiers);
+        unset($userData->user_roles);
+        unset($userData->user_blocks);
+        unset($userData->user_statistics);
+        unset($userData->proxy_for_users);
+
+        $this->debug($userData->asXML());
+
+        // Update user in Alma
+        $this->makeRequest(
+            '/users/' . urlencode($patron['id']),
+            [],
+            [],
+            'PUT',
+            $userData->asXML(),
+            ['Content-Type' => 'application/xml']
+        );
+
+        return [
+            'success' => true,
+            'status' => 'request_change_accepted',
+            'sys_message' => ''
+        ];
+    }
+
+    /**
      * Public Function which retrieves renew, hold and cancel settings from the
      * driver ini file.
      *
@@ -198,5 +454,22 @@ class Alma extends \VuFind\ILS\Driver\Alma
             return $config;
         }
         return parent::getConfig($function, $params);
+    }
+
+    /**
+     * Get Default Pick Up Location
+     *
+     * @param array $patron      Patron information returned by the patronLogin
+     * method.
+     * @param array $holdDetails Optional array, only passed in when getting a list
+     * in the context of placing a hold; contains most of the same values passed to
+     * placeHold, minus the patron data.  May be used to limit the pickup options
+     * or may be ignored.
+     *
+     * @return string       The default pickup location for the patron.
+     */
+    public function getDefaultPickUpLocation($patron = null, $holdDetails = null)
+    {
+        return false;
     }
 }
