cc80191a9155dc1d7e9a3147e74cf950474c3e2f
Ere Maijala <ere.maijala@helsinki.fi>
2026-03-12 09:44:17 +0200

    [FINNA-3933] Add model viewer support to image paginator. (#3466)
    This allows the image paginator to display 3D models too. As a side-effect this fixes navigation between images when there is a model between images as there's no longer a gap that breaks next/previous.
    
    * Fix zoomable image popup. Fix resize on fullscreen toggle.

--- themes/finna2/js/finna-model-viewer.js
+++ themes/finna2/js/finna-model-viewer.js
@@ -939,7 +939,7 @@ class ModelViewerClass extends HTMLElement {
     } else if (this.oldSize) {
       this.size = this.oldSize;
       delete this.oldSize;
-    } else {
+    } else if (this.parentElement) {
       const computed = getComputedStyle(this.parentElement);
       this.size = {
         x: this.parentElement.offsetWidth,
