66b38d1e707c37ee09cd332ef9a22aa0bd9ce2c4
Ere Maijala <ere.maijala@helsinki.fi>
2026-05-15 11:56:48 +0300

    Fix cookie settings link in embedded content info block.

--- themes/finna2/js/finna-cookie-consent-element.js
+++ themes/finna2/js/finna-cookie-consent-element.js
@@ -1,4 +1,4 @@
-/* global VuFind, CookieConsent */
+/* global VuFind */
 class FinnaCookieConsentElement extends HTMLElement {
 
   /**
@@ -103,11 +103,8 @@ class FinnaCookieConsentElement extends HTMLElement {
     aShowModal.classList.add('btn', 'btn-default');
     aShowModal.href = '#';
     aShowModal.setAttribute('aria-haspopup', 'dialog');
+    aShowModal.dataset.cc = 'show-consentModal';
     aShowModal.append(VuFind.translate('Cookie Settings'));
-    aShowModal.addEventListener('click', () => {
-      $.fn.finnaPopup.closeOpen();
-      CookieConsent.showPreferences();
-    });
     divActions.append(aShowModal);
     divInfo.append(divActions);
     this.append(divInfo);
